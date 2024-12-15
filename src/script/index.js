aaf732ac92bb2503a84038bd9a1a0c5f62dd6732
Tederr <jp3351207@gmail.com>
2024-12-14 22:27:20 -0300

    finalizado

--- src/script/index.js
+++ src/script/index.js
@@ -20,3 +20,17 @@ function addEventListenerToCards(){
 }
 
 document.addEventListener("DOMContentLoaded", addEventListenerToCards, false);
+
+function selectCarouselItem(selectedButtonElement){
+    const selectedItem = selectedButtonElement.id;
+    const carousel = document.querySelector('.b-card-carousel');
+    const transform = carousel.style.transform;
+    const rotateY = transform.match(/rotateY\((-?\d+deg)\)/i);
+    const rotateYDeg = -120 * (Number(selectedItem) - 1);
+    const newTransform = transform.replace(rotateY[0], `rotateY(${rotateYDeg}deg)`);
+    carousel.style.transform = newTransform;
+
+    const activeButtonElement = document.querySelector('.b-controller__button--active');
+    activeButtonElement.classList.remove('b-controller__button--active');
+    selectedButtonElement.classList.add('b-controller__button--active');
+}
